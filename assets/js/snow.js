b2c87dbe54647b1bb660b0aa709ff6b9b99a2827
Zihan Mei <meizihanyx@gmail.com>
2020-12-24 02:21:55 +0800

    Christmas snow effect

---
+++ assets/js/snow.js
@@ -0,0 +1,42 @@
+// reference: https://www.cnblogs.com/lmlblogs/p/8732350.html
+(function($){
+	
+	$.fn.snow = function(options){
+	
+			var $flake 			= $('<div id="snowbox" />').css({'position': 'absolute', 'top': '-50px', 'z-index': '9999'}).html('&#10052;'),
+				documentHeight 	= $(document).height(),
+				documentWidth	= $(document).width(),
+				defaults		= {
+									minSize		: 10,		//雪花的最小尺寸
+									maxSize		: 20,		//雪花的最大尺寸
+									newOn		: 1000,		//雪花出现的频率
+									flakeColor	: "#FFFFFF"	
+								},
+				options			= $.extend({}, defaults, options);
+			
+			var interval		= setInterval( function(){
+				var startPositionLeft 	= Math.random() * documentWidth - 100,
+				 	startOpacity		= 0.5 + Math.random(),
+					sizeFlake			= options.minSize + Math.random() * options.maxSize,
+					endPositionTop		= documentHeight - 40,
+					endPositionLeft		= startPositionLeft - 100 + Math.random() * 500,
+					durationFall		= documentHeight * 10 + Math.random() * 5000;
+				$flake.clone().appendTo('div.snow-container').css({
+							left: startPositionLeft,
+							opacity: startOpacity,
+							'font-size': sizeFlake,
+							color: options.flakeColor
+						}).animate({
+							top: endPositionTop,
+							left: endPositionLeft,
+							opacity: 1
+						},durationFall,'linear',function(){
+							$(this).remove()
+						}
+					);
+					
+			}, options.newOn);
+	
+	};
+	
+})(jQuery);
