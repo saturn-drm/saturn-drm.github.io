c6af7da6e98d0a3809a6d710c31ebabc9948428b
Zihan Mei <meizihanyx@gmail.com>
2021-09-09 15:58:55 +0800

    jinja2 articles converter dev

--- app/app.py
+++ app/app.py
@@ -5,14 +5,32 @@ import MDListing
 # load layout folder
 env = Environment(loader=FileSystemLoader(['layout', '.']))
 
-# render 404.html
-template = env.get_template('404.html')
-with open('404.html', 'w') as f:
-    f.write(template.render())
-    f.close()
+# # render 404.html
+# template = env.get_template('404.html')
+# with open('404.html', 'w') as f:
+#     f.write(template.render())
 
-# render home-test.html
-template = env.get_template('home.html')
-with open('home.html', 'w') as f:
-    f.write(template.render())
-    f.close()
+# # render home-test.html
+# template = env.get_template('home.html')
+# with open('home.html', 'w') as f:
+#     f.write(template.render())
+
+# retrieve all the markdown posts
+orip = 'posts'
+desp = 'articles'
+MDPathInstance = MDListing.MDPath(orip, desp)
+MDPathInstance.getFiles()
+print(MDPathInstance.pathdict)
+
+# render article demo
+## TODO: title under content, toc, title of page, some js in template
+fp = 'posts/00projects/2020-01-16-theater-design-wudaokou.md'
+MDPostInstance = MDParsing.MDPost(fp)
+MDPostInstance.htmlParsing()
+MDPostInstance.soupParsing()
+MDPostInstance.modifyHTagAnchor()
+MDPostInstance.modifyIMGPath()
+MDPostInstance.modifyTableHead()
+template = env.get_template('article-dev.html')
+with open('articles/test.html', 'w') as f:
+    f.write(template.render(headerimg='/assets/img/covers/codingcover.jpg', posthtml=MDPostInstance.soup))
